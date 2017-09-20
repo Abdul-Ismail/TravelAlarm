a7496b006be1117b450904db3870407a437e4308
Abdul-Ismail <abduul.ismail@gmail.com>
2017-09-20 14:33:02 +0100

    Search location on map

--- MapSearchViewController.swift
+++ MapSearchViewController.swift
@@ -7,32 +7,86 @@
 //
 
 import UIKit
+import MapKit
 
-class MapSearchViewController: UIViewController {
+class MapSearchViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var getCurrentLocation: UIButton!
     
     @IBAction func getCurrentLocationAction(_ sender: Any) {
     }
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapSearch: UISearchBar!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapSearch.delegate = self
+        
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
     }
-    */
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        //dismiss keyboard
+        view.endEditing(true)
+        
+        //ignoring user
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        //Activity indicator
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.stopAnimating()
+        
+        self.view.addSubview(activityIndicator)
+        
+        //create search request
+        let searchRequest = MKLocalSearchRequest()
+        searchRequest.naturalLanguageQuery = searchBar.text
+        
+        //start search based on that search request
+        let activeSearch = MKLocalSearch(request: searchRequest)
+        activeSearch.start { (result, error) in
+           //remove activity indicator
+            activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            
+            if result == nil {
+                print("error")
+            }else {
+               //getting data
+                let latitude = result?.boundingRegion.center.latitude
+                let longitude = result?.boundingRegion.center.longitude
+                
+                //Create annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = searchBar.text
+                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                self.mapView.addAnnotation(annotation) //add to map
+                
+                //zoom in at location
+                let coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                let span = MKCoordinateSpanMake(0.1, 0.1) //how zoomed in
+                let region = MKCoordinateRegionMake(coordinate, span)
+                self.mapView.setRegion(region, animated: true)
+                
+                
+                
+            }
+        }
+        
+    }
 
 }
+
+

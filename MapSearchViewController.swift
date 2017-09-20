8e09913804c439c5b608660e83f9c055051b2d9b
Abdul-Ismail <abduul.ismail@gmail.com>
2017-09-20 13:35:22 +0100

    Storyboard design completed

---
+++ MapSearchViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapSearchViewController.swift
+//  TravelAlarm
+//
+//  Created by Abdulaziz Ismail on 20/09/2017.
+//  Copyright © 2017 Abdulaziz Ismail. All rights reserved.
+//
+
+import UIKit
+
+class MapSearchViewController: UIViewController {
+    @IBOutlet weak var getCurrentLocation: UIButton!
+    
+    @IBAction func getCurrentLocationAction(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TravelAlarm/PinnedLocationTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PinnedLocationTableViewCell.swift
+//  TravelAlarm
+//
+//  Created by Abdulaziz Ismail on 20/09/2017.
+//  Copyright © 2017 Abdulaziz Ismail. All rights reserved.
+//
+
+import UIKit
+
+class PinnedLocationTableViewCell: UITableViewCell {
+    @IBOutlet weak var pinImage: UIImageView!
+    
+    @IBOutlet weak var pinLocation: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
